bf436bc3ab790ca7ac2277434c0dd737d34733bf
huahua.luo <huahua.luo@guanaitong.com>
2017-09-30 14:45:33 +0800

    test

---
+++ src/main/java/com/smartzhe/main/Main.java
@@ -0,0 +1,13 @@
+package com.smartzhe.main;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: luohuahua
+ * Date: 2017/9/30
+ * Time: 14:23  .
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
